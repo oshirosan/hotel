d902a2f9b6660c7682a005b851fee216faa14dc1
oshirosan <oshiro0320@gmail.com>
2021-09-19 13:15:14 +0900

    first commit

---
+++ js/function.js
@@ -0,0 +1,21 @@
+
+  
+$(function(){
+
+  // トップページスライドショー
+  $(".slide-show").slick({
+      autoplay:true,
+      dots:true,
+      autoplaySpeed:2500,
+      fade: true,
+      arrows:false,
+      infinite: true,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      pauseOnDotsHover: false
+  });
+
+  
+
+});
+
